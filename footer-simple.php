8fe16bb014607204ddb06a545d0a940f7f68d608
Whornat <ecrire@whornat.com>
2017-05-20 09:58:09 +0200

    --

--- footer-simple.php
+++ footer-simple.php
@@ -9,6 +9,7 @@
  * @package wntblank_theme_boot4
  */
 ?>
+<div class="container" id="simplefooter">
 			<?php 
 			$infostheme = wp_get_theme();
 			$nomdusite = get_bloginfo( 'name' );
@@ -17,6 +18,7 @@
 			?>
 			
 <p class="credits">Une Conception <a href="<?php echo $infostheme->get( 'AuthorURI' ); ?>" target="blank" class="author_site"><?php echo $infostheme->get( 'Author' ); ?></a> pour <a href="<?php echo $site_url; ?>" class="client_site" title="<?php echo $descriptiondusite; ?>"><?php echo $nomdusite; ?></a> <span class="pipe">|</span> <a href="<?php echo get_permalink( get_page_by_title( 'Mentions légales' ) ); ?>" class="mentionslegales">mentions légales</a> <span class="pipe">|</span> <a href="<?php echo get_permalink( get_page_by_title( 'Plan du site' ) ); ?>" class="plandusite" title="plan du site">Plan du site</a></p>
+</div>
 
 <?php wp_footer(); ?>
 </body>
