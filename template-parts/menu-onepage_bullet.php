8ec3cfbfe995a25628c6bcbab276bef70813596a
Whornat <ecrire@whornat.com>
2017-11-02 11:29:10 +0100

    -- MODIFICATION onepage bullet nav : use tooltips ---

--- template-parts/menu-onepage_bullet.php
+++ template-parts/menu-onepage_bullet.php
@@ -1,8 +1,25 @@
 <nav id="scrollingnav" class="navscroll">
-	     <ul class="nav navbar-nav navbar-dotted">
-			  <li class="nav-item"><a class="nav-link" href="#step1"><span class="nav-dot"></span><span class="nav-label">Intro</span></a></li>
-			  <li class="nav-item"><a class="nav-link" href="#step2"><span class="nav-dot"></span><span class="nav-label">Intro</span></a></li>
-			  <li class="nav-item"><a class="nav-link" href="#step3"><span class="nav-dot"></span><span class="nav-label">Intro</span></a></li>
-			  <li class="nav-item"><a class="nav-link" href="#step4"><span class="nav-dot"></span><span class="nav-label">Intro</span></a></li>
+	     <ul class="navbar-dotted">
+			  <li class="nav-item">
+				  <a class="nav-link" href="#stepintro"  data-toggle="tooltip" data-placement="left" title="1"><span class="nav-dot"> </span></a>
+			 </li>
+			<li class="nav-item">
+				  <a class="nav-link" href="#step01"  data-toggle="tooltip" data-placement="left" title="2"><span class="nav-dot"> </span></a>
+			 </li>
+			<li class="nav-item">
+				  <a class="nav-link" href="#step02"  data-toggle="tooltip" data-placement="left" title="3"><span class="nav-dot"> </span></a>
+			 </li>
+			<li class="nav-item">
+				  <a class="nav-link" href="#step03"  data-toggle="tooltip" data-placement="left" title="4"><span class="nav-dot"> </span></a>
+			 </li>
+			<li class="nav-item">
+				  <a class="nav-link" href="#step04"  data-toggle="tooltip" data-placement="left" title="5"><span class="nav-dot"> </span></a>
+			 </li> 
 	  	</ul>
 </nav>
+
+<script type="text/javascript">
+$(function () {
+   $('#scrollingnav .nav-link').tooltip()
+})
+</script>
