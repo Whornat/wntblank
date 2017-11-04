68f8bb09c4151d08b0679a60052ea3bdc82ab6be
Whornat <ecrire@whornat.com>
2017-11-04 12:24:23 +0100

    -- correction bullet nav --

--- template-parts/menu-onepage_bullet.php
+++ template-parts/menu-onepage_bullet.php
@@ -1,19 +1,16 @@
 <nav id="scrollingnav" class="navscroll">
 	     <ul class="navbar-dotted">
-			  <li class="nav-item">
-				  <a class="nav-link" href="#stepintro"  data-toggle="tooltip" data-placement="left" title="1"><span class="nav-dot"> </span></a>
+			<li class="nav-item">
+				  <a class="nav-link" href="#step1"  data-toggle="tooltip" data-placement="left" title="1"><span class="nav-dot"> </span></a>
 			 </li>
 			<li class="nav-item">
-				  <a class="nav-link" href="#step01"  data-toggle="tooltip" data-placement="left" title="2"><span class="nav-dot"> </span></a>
+				  <a class="nav-link" href="#step2"  data-toggle="tooltip" data-placement="left" title="2"><span class="nav-dot"> </span></a>
 			 </li>
 			<li class="nav-item">
-				  <a class="nav-link" href="#step02"  data-toggle="tooltip" data-placement="left" title="3"><span class="nav-dot"> </span></a>
+				  <a class="nav-link" href="#step3"  data-toggle="tooltip" data-placement="left" title="3"><span class="nav-dot"> </span></a>
 			 </li>
 			<li class="nav-item">
-				  <a class="nav-link" href="#step03"  data-toggle="tooltip" data-placement="left" title="4"><span class="nav-dot"> </span></a>
-			 </li>
-			<li class="nav-item">
-				  <a class="nav-link" href="#step04"  data-toggle="tooltip" data-placement="left" title="5"><span class="nav-dot"> </span></a>
+				  <a class="nav-link" href="#step4"  data-toggle="tooltip" data-placement="left" title="4"><span class="nav-dot"> </span></a>
 			 </li> 
 	  	</ul>
 </nav>
