8a4bdf63107d7d37c141949eb3822893c3eab531
Whornat <ecrire@whornat.com>
2017-11-21 12:30:16 +0100

    -- ADD ROW to sidebar --

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 <?php if (get_theme_mod('sidebar_size') == ''){ $sidebar_size = "col-lg-3"; }else{ $sidebar_size = get_theme_mod('sidebar_size');} ?>
 <aside id="secondary" class="<?php echo $sidebar_size; ?> widget-area" role="complementary">
+	<div class="row">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>	
 </aside><!-- #secondary -->
