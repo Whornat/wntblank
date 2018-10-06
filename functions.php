665200e107390b69b106af5fb79373d7c1c0f5a2
Whornat <ecrire@whornat.com>
2018-10-06 14:02:56 +0200

    Custom-header desactivé

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ add_action('acf/init', 'my_acf_init');
 
 
 /** Implement the Custom Header feature.*/
-require get_template_directory() . '/inc/custom-header.php';
+//require get_template_directory() . '/inc/custom-header.php';
 /** Custom template tags for this theme. */
 require get_template_directory() . '/inc/template-tags.php';
 /** Custom functions that act independently of the theme templates. */
