f81ee88c2b079806aeb278d18648e264445ec377
Whornat <ecrire@whornat.com>
2018-11-13 20:52:05 +0100

    <span class="labelmenu"></span>

--- template-parts/menu-topfixed.php
+++ template-parts/menu-topfixed.php
@@ -15,9 +15,8 @@
    	<?php } ?>
 	  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span> <?php _e( 'Display menu', 'wntblanktheme' ); ?>
+    <span class="navbar-toggler-icon"></span> <span class="labelmenu"><?php _e( 'Display menu', 'wntblanktheme' ); ?></span>
   </button>		  
-	  
 	  
 
          <?php
